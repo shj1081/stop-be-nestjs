f38a2fa6a058a3f1440ded02e4a1a120927b1376
hyzoon <shj83811081@gmail.com>
2024-08-20 02:49:01 +0900

    feat: impl swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -10,17 +13,37 @@ async function bootstrap() {
       transformOptions: {
         enableImplicitConversion: true,
       },
-    }),
+    })
   );
 
   app.enableCors({
-    origin: '*',
+    origin: "*",
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ["GET", "POST", "PUT", "DELETE"],
   });
-
   app.enableShutdownHooks();
 
-  await app.listen(4000);
+  const appConfig = app.get(ConfigService); // ConfigService를 사용하기 위해 appConfig를 만들어줌
+
+  const config = new DocumentBuilder()
+    .setTitle("stop-be-clone")
+    .setDescription("API Description")
+    .setVersion("1.0")
+    .addTag("API")
+    .addBearerAuth(
+      {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+        in: "header",
+      },
+      "access-token"
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api-docs", app, document);
+
+  await app.listen(appConfig.get("app.port"));
+  console.log(`==== Running as ${process.env.APP_ENV} ====`);
 }
 bootstrap();
