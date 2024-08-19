8a27be613a140d518baaf7a65be936e636e7f7a7
hyzoon <shj83811081@gmail.com>
2024-08-20 00:33:38 +0900

    feat: modify main.ts
    * npm i --save class-validator class-transformer
    * globalPipes
    * enableCors
    * enableShutdownHooks

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  });
+
+  app.enableShutdownHooks();
+
   await app.listen(4000);
 }
 bootstrap();
